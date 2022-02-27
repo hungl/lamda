52708052cdd302a170f04e82b7bb721d97c02150
unknown <hungqlac@gmail.com>
2022-02-26 23:11:08 -0500

    first commit

---
+++ signin_lamda/ConnectionManager.js
@@ -0,0 +1,10 @@
+module.exports=function(){
+    this.dbConnections = [];
+    this.dbConnections['authDatabase'] = {
+         host: 'database-1.czpleqf21hdj.us-east-1.rds.amazonaws.com',
+         port: 3306,
+         user: 'admin',
+         password: 'password1',
+         database: 'imts_db'
+        };
+     };
